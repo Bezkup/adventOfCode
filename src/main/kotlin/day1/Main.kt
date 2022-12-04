28131ae396b253540736c6c985800ee69de1c514
Bezkup <bruroy96@gmail.com>
2022-12-04 13:15:39 +0100

    Init repo Added Day 1 and Day 2 advent of code

---
+++ src/main/kotlin/day1/Main.kt
@@ -0,0 +1,40 @@
+package day1
+
+import readInput
+
+fun main() {
+    fun part1(input : List<String>) : Int {
+        val listOfCaloriesElf = mutableListOf<Int>()
+        var countCalories = 0
+        input.iterator().forEach {
+            x -> if (x != "") {countCalories += Integer.parseInt(x)} else {listOfCaloriesElf.add(countCalories); countCalories = 0}
+        }
+        return listOfCaloriesElf.max()
+    }
+
+    fun part2(input : List<String>) : Int {
+        val listOfCaloriesElf = mutableListOf<Int>()
+        var countCalories = 0
+        input.forEach {
+                x -> if (x != "") {countCalories += Integer.parseInt(x) } else { listOfCaloriesElf.add(countCalories); countCalories = 0 }
+        }
+        var firstElf = 0
+        var secondElf = 0
+        var thirdElf = 0
+        listOfCaloriesElf.forEach {
+            x -> run {
+                if (x > firstElf)
+                    firstElf = x
+                else if (x > secondElf)
+                    secondElf = x
+                else if (x > thirdElf)
+                    thirdElf = x
+            }
+        }
+        return firstElf + secondElf + thirdElf
+    }
+
+    val input = readInput("day1")
+    println(part1(input))
+    println(part2(input))
+}
---
+++ src/main/kotlin/day2/Main.kt
@@ -0,0 +1,41 @@
+package day2
+
+import readInput
+
+fun main() {
+
+    fun getGames(input: List<String>): List<Pair<Char, Char>> {
+        return input.map { it.split(" ") }
+            .map { it[0][0] to it[1][0] }
+    }
+
+    fun part1(input: List<String>): Int {
+        return getGames(input)
+            .map { (it.first - 'A' + 1) to (it.second - 'X' + 1) }
+            .sumOf {
+                it.second +
+                        (when {
+                            it.second - it.first == 1 || it.second - it.first == -2 -> 6
+                            it.second - it.first == 0 -> 3
+                            else -> 0
+                        })
+            }
+    }
+
+    fun part2(input: List<String>): Int {
+        return getGames(input)
+            .map { (it.first - 'A' + 1) to ((it.second - 'X') * 3) }
+            .sumOf {
+                it.second +
+                        (when (it.second) {
+                            6 -> if (it.first + 1 > 3) 1 else it.first + 1
+                            3 -> it.first
+                            else -> if (it.first - 1 == 0) 3 else it.first - 1
+                        })
+            }
+    }
+
+    val input = readInput("day2")
+    println(part1(input))
+    println(part2(input))
+}
